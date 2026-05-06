994f52acfca16ead8665ca8cc9d5edee287d29c4
prashanthrangu7-ux <prashanthrangu7@gmail.com>
2026-05-07 01:02:49 +0530

    Enable credentials in CORS headers
    Add Access-Control-Allow-Credentials header for CORS.

--- api/chat.js
+++ api/chat.js
@@ -3,6 +3,10 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+  "Access-Control-Allow-Credentials",
+  "true"
+);
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
